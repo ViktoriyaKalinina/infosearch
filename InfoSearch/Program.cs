06f345f7a44034261bcd92d7e07f23270612e1b6
Vika Kalinina <vsq.kalinina@gmail.com>
2019-03-15 20:58:54 +0300

    fixed

--- InfoSearch/Program.cs
+++ InfoSearch/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using HtmlAgilityPack;
 
 namespace InfoSearch
 {
@@ -9,18 +10,35 @@ namespace InfoSearch
         static void Main(string[] args)
         {
             string docPath = @"/Users/visqas/infosearch/web_pages";
+            string resultDoc;
 
-            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "index.txt"), true))
-            using (var client = new WebClient())
+            HtmlDocument htmlDoc = new HtmlDocument();
+
+            int counter = 100;
+            for (int i = 1; i <= counter; i++)
             {
-                for (int i = 1; i <= 100; i++)
+                string remoteUrl = String.Format("http://online-knigi.com/page/196339?page={0}", i);
+                string fileName = String.Format(@"/Users/visqas/infosearch/web_pages/{0}page.txt", i);
+
+                var request = (HttpWebRequest)WebRequest.Create(remoteUrl);
+                request.Method = "GET";
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "index.txt"), true))
                 {
-                    string remoteUrl = String.Format("http://online-knigi.com/page/196339?page={0}", i);
-                    string fileName = String.Format(@"/Users/visqas/infosearch/web_pages/page{0}.html", i);
+                    using (var response = (HttpWebResponse)request.GetResponse())
+                    {
+                        using (var stream = response.GetResponseStream())
+                        {
+                            using (StreamWriter file = new StreamWriter(fileName))
+                            {
+                                htmlDoc.Load(stream);
+                                resultDoc = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]/div[2]/div/div[3]").InnerText;
 
-                    client.DownloadFile(remoteUrl, fileName);
-
-                    outputFile.WriteLine("doc: {0}, url: {1}", i, remoteUrl);
+                                file.WriteLine(resultDoc);
+                                outputFile.WriteLine("doc: {0}, url: {1}", i, remoteUrl);
+                            }
+                        }
+                    }
                 }
             }
         }
