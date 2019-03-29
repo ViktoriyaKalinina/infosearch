9df1da743ffea76616307e38dcb92d2b2bdfdc7c
Vika Kalinina <vsq.kalinina@gmail.com>
2019-03-29 17:41:51 +0300

    second hw

--- inf_py/lemmatisation.py
+++ inf_py/lemmatisation.py
@@ -1,15 +1,28 @@
 import pymorphy2
 import re, string
 morph = pymorphy2.MorphAnalyzer()
+counter = 10
+i = 1
 
-f = open("text.txt")
-text = f.read()
+def remove_punctuation(text):
+  text = re.sub(r'\W', " ", text)
+  processed_txt = re.sub(" +", " ", text)
+  return processed_txt
 
-txt_wo_punctuation = re.sub(r'\W', " ", text)
-txt_wo_punctuation = re.sub(" +", " ", txt_wo_punctuation)
-words = txt_wo_punctuation.split(' ')
+while i < counter:
+  read_file = ("/Users/visqas/infosearch/inf_dotnet/web_pages/%dpage.txt" % i)
+  write_file = ("/Users/visqas/infosearch/inf_py/lemmatisation_files/%doutput.txt" % i)
+  
+  f = open(read_file)
+  text = f.read()
+  txt_wo_punctuation = remove_punctuation(text)
+  words = txt_wo_punctuation.split(' ')
 
-o = open("output_text", "w")
-for word in words:
-    p = morph.parse(word)[0]
-    o.write(p.normal_form + "\n")
+  o = open(write_file, "w")
+    
+  for word in words:
+      p = morph.parse(word)[0]
+      o.write(p.normal_form + "\n")
+      f.close()
+  
+  i = i + 1
